4c87eac771f9a3fa56d783dbca443b3927f1fc0e
THEAbhishekjoshi <abhishekjoshi.739.ak@gmail.com>
2026-07-31 12:41:11 +0530

    feat(chat): add auto-resizing textarea with scroll limit

--- Frontend/src/components/ChatBot.tsx
+++ Frontend/src/components/ChatBot.tsx
@@ -338,85 +338,95 @@ const ChatBot = () => {
     return (
         <div className={`flex flex-col items-center ${(allMessages.length > 0 && allMessages[0].role.length > 0) ? '' : 'justify-center'} mx-auto w-full h-full bg-[#3F424A] text-white px-2 sm:px-6 md:px-10 `}>
             {/* chats */}
-            {(allMessages.length > 0 && allMessages[0].role.length > 0) ? <div
-                className="w-full lg:w-3/4 mt-2 flex-1 py-5 pb-24"
-            // style={{ height: "calc(100vh - 8rem)" }}
-            >
-                {allMessages.map((m, i) => (
-                    <div key={i} className="mb-2">
+            {(allMessages.length > 0 && allMessages[0].role.length > 0) ?
+                <div className="w-full lg:w-3/4 mt-2 flex-1 py-5 pb-24">
+                    {allMessages.map((m, i) => (
+                        <div key={i} className="mb-2">
 
-                        {/* Role Header */}
-                        <div className="font-semibold">
-                            {m.role === "user" ? (
-                                <div className="flex gap-3">
-                                    <img src={user} alt="user" className="w-5 h-5 rounded-full" />
-                                    You
-                                </div>
-                            ) : (
-                                <div className="flex gap-3">
-                                    <img src={logo} alt="bot" className="w-5 h-5 rounded-full" />
-                                    Response
-                                </div>
-                            )}
-                        </div>
+                            {/* Role Header */}
+                            <div className="font-semibold">
+                                {m.role === "user" ? (
+                                    <div className="flex gap-3">
+                                        <img src={user} alt="user" className="w-5 h-5 rounded-full" />
+                                        You
+                                    </div>
+                                ) : (
+                                    <div className="flex gap-3">
+                                        <img src={logo} alt="bot" className="w-5 h-5 rounded-full" />
+                                        Response
+                                    </div>
+                                )}
+                            </div>
 
-                        {/* Message Content */}
-                        <div
-                            className={`
+                            {/* Message Content */}
+                            <div
+                                className={`
                             inline-block p-5 rounded-lg wrap-break-word text-[0.9rem]
                             max-w-[100%] text-justify
                             ${m.role === "user" ? "bg-[#4b4f5b]" :
-                                    m.content.length > 0 ? "bg-[#282f3f] "
-                                        : "bg-[#282f3f] animate-pulse "
-                                }
+                                        m.content.length > 0 ? "bg-[#282f3f] "
+                                            : "bg-[#282f3f] animate-pulse "
+                                    }
                             `}
-                        >
-                            {/^(http|https):\/\//.test(m.content) ? (
+                            >
+                                {/^(http|https):\/\//.test(m.content) ? (
 
-                                <img src={m.content} className="rounded" />
+                                    <img src={m.content} className="rounded" />
 
-                            ) : (
-                                m.role === "assistant" ?
-                                    <div className="flex flex-col">
-                                        <div ref={messageRef}>{m.content}</div>
-                                        {m.messageId ?
-                                            <div className="mt-4 flex gap-3 justify-end">
-                                                <div className="text-[0.7rem] bg-[#202633] rounded-md p-2 hover:bg-[#121722] cursor-pointer">
-                                                    <button className="flex items-center gap-1" onClick={() => {
-                                                        if (m.messageId) {
+                                ) : (
+                                    m.role === "assistant" ?
+                                        <div className="flex flex-col">
+                                            <div ref={messageRef}>{m.content}</div>
+                                            {m.messageId ?
+                                                <div className="mt-4 flex gap-3 justify-end">
+                                                    <div className="text-[0.7rem] bg-[#202633] rounded-md p-2 hover:bg-[#121722] cursor-pointer">
+                                                        <button className="flex items-center gap-1" onClick={() => {
+                                                            if (m.messageId) {
 
-                                                            handleGenereateResponse(m.messageId)
+                                                                handleGenereateResponse(m.messageId)
 
-                                                        }
-                                                        else {
-                                                        }
-                                                    }}><RefreshCcw size={12} /><span className="hidden sm:inline-block">Generate Response</span>
-                                                    </button>
-                                                </div>
-                                                <div className="text-[0.7rem] bg-[#202633] rounded-md p-2 hover:bg-[#121722] cursor-pointer">
-                                                    <button className="flex items-center gap-1 " onClick={() => handleCopy(m.content)}>
-                                                        <Copy size={12} /><span className="hidden sm:inline-block">Copy</span>
-                                                    </button>
-                                                </div>
-                                            </div> : <div></div>
-                                        }
-                                    </div> : m.content
-                            )}
+                                                            }
+                                                            else {
+                                                            }
+                                                        }}><RefreshCcw size={12} /><span className="hidden sm:inline-block">Generate Response</span>
+                                                        </button>
+                                                    </div>
+                                                    <div className="text-[0.7rem] bg-[#202633] rounded-md p-2 hover:bg-[#121722] cursor-pointer">
+                                                        <button className="flex items-center gap-1 " onClick={() => handleCopy(m.content)}>
+                                                            <Copy size={12} /><span className="hidden sm:inline-block">Copy</span>
+                                                        </button>
+                                                    </div>
+                                                </div> : <div></div>
+                                            }
+                                        </div> : m.content
+                                )}
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-                : <div className="text-xl sm:text-xl md:text-2xl lg:text-3xl mb-4">What's on your mind today?</div>}
+                    ))}
+                </div>
+                :
+                <div className="text-xl sm:text-xl md:text-2xl lg:text-3xl mb-4 text-center flex-1 flex items-center md:flex-none md:block">
+                    What's on your mind today?
+                </div>
+            }
 
-            <div className="w-full lg:w-3/4 sticky bottom-0 bg-[#3F424A] py-3 z-10">
-                <div className="flex items-center sm:gap-3 bg-[#2b2c30] px-1 sm:px-4 py-3 rounded-xl border border-[#3a3b3f]">
+            <div className="w-full lg:w-4/6 sticky bottom-0 bg-[#3F424A] py-3 z-10">
+                <div className="flex flex-col md:flex-row md:items-center gap-3 bg-[#2b2c30] px-1 sm:px-4 py-3 rounded-xl border border-[#3a3b3f]">
                     {/* Text input */}
                     <textarea
                         rows={1}
                         placeholder="Ask questions..."
                         value={userMessage}
-                        className="flex-1 bg-transparent outline-none text-white placeholder-gray-400 text-sm resize-none"
-                        onChange={(e) => setUserMessage(e.target.value)}
+                        className="flex-1 bg-transparent outline-none text-white placeholder-gray-400 text-sm resize-none p-1 leading-6 max-h-[72px] overflow-y-auto"
+                        onChange={(e) => {
+
+                            setUserMessage(e.target.value)
+
+                            e.target.style.height = "auto"
+
+                            e.target.style.height = `${Math.min(e.target.scrollHeight, 72)}px`
+
+                        }}
                         onKeyDown={(e) => {
                             if (e.key === "Enter" && !e.shiftKey) {
                                 e.preventDefault()
@@ -425,27 +435,29 @@ const ChatBot = () => {
                         }}
                         disabled={typing ? false : true}
                     />
-                    {/* Send Button */}
-                    <button className="p-2 rounded-lg hover:bg-[#3a3b3f] transition"
-                        onClick={() => sendButton()}>
-                        <Send size={18} className="text-gray-300" />
-                    </button>
-                    {/* Mic Button */}
-                    <button
-                        className={`p-2 rounded-lg transition ${isTranscribing ? "cursor-not-allowed opacity-50" : "hover:bg-[#3a3b3f]"}`}
-                        disabled={isTranscribing}
-                        onClick={() =>
-                            setIsRecording(prev => !prev)
-                        }
-                    >
-                        {isTranscribing ? (
-                            <div className=" w-5 h-5 rounded-full border-2 border-gray-300 border-t-transparent animate-spin" />
-                        ) : isRecording ? (
-                            <Square size={20} className="text-red-400 animate-pulse" />
-                        ) : (
-                            < Mic size={20} className="text-gray-300" />
-                        )}
-                    </button>
+                    <div className="flex justify-end shrink-0">
+                        {/* Send Button */}
+                        <button className="p-2 rounded-lg hover:bg-[#3a3b3f] transition"
+                            onClick={() => sendButton()}>
+                            <Send size={18} className="text-gray-300" />
+                        </button>
+                        {/* Mic Button */}
+                        <button
+                            className={`p-2 rounded-lg transition ${isTranscribing ? "cursor-not-allowed opacity-50" : "hover:bg-[#3a3b3f]"}`}
+                            disabled={isTranscribing}
+                            onClick={() =>
+                                setIsRecording(prev => !prev)
+                            }
+                        >
+                            {isTranscribing ? (
+                                <div className=" w-5 h-5 rounded-full border-2 border-gray-300 border-t-transparent animate-spin" />
+                            ) : isRecording ? (
+                                <Square size={20} className="text-red-400 animate-pulse" />
+                            ) : (
+                                < Mic size={20} className="text-gray-300" />
+                            )}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
